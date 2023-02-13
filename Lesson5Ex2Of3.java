3130097c3cb5ce4ff36ff601a6ba7721f755d6d3
Myreen Leanca <124058246+mleanca@users.noreply.github.com>
2023-02-13 18:40:19 +0200

    Changes 130223

--- Lesson5Ex2Of3.java
+++ Lesson5Ex2Of3.java
@@ -2,12 +2,22 @@ public class Lesson5Ex2Of3 {
     public static void main(String[] args) {
 
         for (int i = 100; i <= 1000; i++) {
+            //1st condition, begins at 100
+            //2nd condition, values must be less than or equal 1000
+            //increment by 1 unit, i = i + 1
 
             if (i % 5 == 0 && i % 6 == 0)
                 System.out.print(i + " ");
+            // 100 % 5 == 0 && 100 % 6 == 0
+            // 100 % 5 is 0 because it divides evenly
+            // 100 % 6 is 4
+            // 0 == 0 && 4 == 0, it is false
+            //every increment will be false until a number it is divisible by 5 and 6
 
             if (i % 10 == 0)
                 System.out.println();
+            // (100 % 10 == 0)
+            // outputs as space
         }
     }
 }
--- Lesson5Ex3Of3.java
+++ Lesson5Ex3Of3.java
@@ -1,10 +1,17 @@
 public class Lesson5Ex3Of3 {
     public static void main(String[] args) {
 
-        double sum = 0.0;
+        double sum = 0.0; //to initialize sum variable
 
         for (double i = 1.0; i <= 97.0; i += 2) {
-            sum += i / (i + 2);
+            //1st condition, i begins at 1
+            //2nd condition, i must be less than or equal to 97
+            //increment byt 2
+            sum += (i / (i + 2));
+            //sum = sum + (i / (i + 2))
+            //1st it sum = 0 + (1 / (1 + 2)) = 1/3
+            //2nd it sum = 0 + (3 / (3 + 2)) = 3/5, sum doesn't increment with each iteration only i
+            //.. n it sum = 0 + (97 / (97 + 2)) = 97/99
         }
         System.out.println("Sum is " + sum);
     }
